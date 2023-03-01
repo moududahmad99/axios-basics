5ea13b7eb7ebd9770dc9e7ec90ae2917d9d0b887
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-03-01 22:20:36 +0600

    axios basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        Axios()
+    })
+
+    const [api, setData] = useState([])
+
+    const Axios = async () => {
+        try {
+            let { data } = await axios.get("https://jsonplaceholder.typicode.com/comments")
+            setData(data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <>
+            {
+                api.map((item) => (
+                    <h4 key={item.email}>{item.email}</h4>
+                ))
+            }
+        </>
+    );
 }
 
 export default App;
